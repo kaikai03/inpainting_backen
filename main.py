598c16586b9ab03da51269e50cf078029cbfe7ec
kai_kai03 <kai_kai03@hotmail.com>
2020-05-18 00:02:58 +0800

    learning，completed

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from typing import List
-from fastapi import FastAPI, Path, Query, Header, Request
+from fastapi import FastAPI, Path, Query, Header, Request, status, File, UploadFile, Form, HTTPException
+# from fastapi.responses import HTMLResponse
+# from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from fastapi.exception_handlers import ( http_exception_handler, request_validation_exception_handler, )
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from fastapi.encoders import jsonable_encoder
+
 import uvicorn as u
 import time
 from pydantic import BaseModel
@@ -28,14 +37,29 @@ async def my_middleware(request: Request, call_next):
     return response
 
 
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"OMG! An HTTP error!: {exc}")
+    return await http_exception_handler(request, exc)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f"OMG! The client sent invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
+
+# raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+
+
 @app.options("/")
 async def options():
     return {"message": "options"}
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def root():
-    return {"message": "Hello World"}
+    # 使用Response包装时，将无视装饰器中的status
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"message": "Hello World"})
 
 
 @app.post("/p")
@@ -63,10 +87,58 @@ class Item(BaseModel):
     tax: float = None
     tags: List[str] = []
 
-
+fake_db = {}
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item):
+    if item is None:
+        raise HTTPException(status_code=405, detail="Item not found", headers={"X-Error": "There goes my error"})
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
     return item
+
+
+@app.get("/index")
+async def main():
+    content = """
+            <body>
+            <form action="/files/" enctype="multipart/form-data" method="post">
+            <input name="files" type="file" multiple>
+            <input type="submit">
+            </form>
+            <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+            <input name="files" type="file" multiple>
+            <input type="submit">
+            </form>
+            <form action="/files3/" enctype="multipart/form-data" method="post">
+            <input name="file" type="file">
+            <input name="fileb" type="file">
+            <input name="token" type="text">
+            <input type="submit">
+            </form>
+            </body>
+             """
+    return HTMLResponse(content=content)
+
+
+@app.post("/files/")
+async def create_files(files: List[bytes] = File(...)):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+    # filename  content_type  file   await{write(data), read(size), seek(offset), close()}
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.post("/files3/")
+async def create_file(
+ file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)):
+    return {
+        "file_size": len(file),
+        "token": token,
+        "fileb_content_type": fileb.content_type,
+    }
 
 
 if __name__ == '__main__':
