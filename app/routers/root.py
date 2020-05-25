7eef0cf00945e9b05403f6c28f69e31a4dd249eb
kai_kai03 <kai_kai03@hotmail.com>
2020-05-26 02:11:38 +0800

    test pydantic about generate object

--- app/routers/root.py
+++ app/routers/root.py
@@ -1,4 +1,5 @@
 from typing import List
+from typing import Union
 from fastapi import FastAPI, Path, Query, Header, Request, status, File, UploadFile, Form, HTTPException
 # from fastapi.responses import HTMLResponse
 from starlette.responses import HTMLResponse, JSONResponse
@@ -16,12 +17,14 @@ router = APIRouter()
 
 
 class video_item(BaseModel):
+    id: int = None
     index: int = None
     name: str = None
-    type: str = 0
+    type: Union['pic', 'video', 'error'] = 'pic'
     description: str = None
+    error: str = None
     cover: str
-    src: str = None
+    src: str
 
 
 @router.get("/random_final/{count}", response_model=List[video_item])
@@ -29,7 +32,6 @@ async def read_all(*, count: int):
     t = video_item(index=0, cover='http://127.0.0.1:90/imgs/1.jpg',src='http://127.0.0.1:90/v/1.mp4')
     t2 = video_item(index=1, cover='http://127.0.0.1:90/imgs/2.jpg',src='http://127.0.0.1:90/v/2.mp4')
     t3 = video_item(index=2, cover='http://127.0.0.1:90/imgs/3.gif', src='http://127.0.0.1:90/v/3.mp4')
-
     return [t,t2,t3]
 
 
@@ -39,14 +41,18 @@ async def test(*, count: int):
     #             {"index":1,"type":"1","img":"http://127.0.0.1:90/imgs/2.jpg","media":"http://127.0.0.1:90/v/2.mp4"},
     #             {"index":2,"type":"1","img":"http://127.0.0.1:90/imgs/3.gif","media":"http://127.0.0.1:90/v/3.mp4"}]
     # return test_
-    t = video_item(index=0, cover='http://127.0.0.1:90/imgs/1.jpg', src='http://127.0.0.1:90/v/1.mp4')
+    t = video_item(cover='http://127.0.0.1:90/imgs/1.jpg', src='http://127.0.0.1:90/v/1.mp4')
     t2 = video_item(index=1, cover='http://127.0.0.1:90/imgs/2.jpg', src='http://127.0.0.1:90/v/2.mp4')
     t3 = video_item(index=2, cover='http://127.0.0.1:90/imgs/3.gif', src='http://127.0.0.1:90/v/3.mp4')
+    t.dict()
+    t.dict(exclude_none=True)
+    t.json(exclude_defaults=True)
+    video_item(**dict({"cover": "http://127.0.0.1:90/imgs/1.jpg", "src": "http://127.0.0.1:90/v/1.mp4"}))
     return [t, t2, t3]
 
 @router.get("/dbtest/")
 async def init_db():
-    con.global_db.history
+    con.global_db.completed.insert()
     return "ok"
 
 
