70c2673173f7b81dedb1435e4c0ea4e6a1d5a28b
kai_kai03 <kai_kai03@hotmail.com>
2020-10-05 23:31:16 +0800

    add heart beat start

--- factory/inpainting_task.py
+++ factory/inpainting_task.py
@@ -91,15 +91,15 @@ def get_online_worker(app_celery):
     ## 这样可以拿到在线列表
     # 不过这里效率有问题，需要异步
     # 有个前提：work必须比app上线得晚
-    return list(app_celery.control.inspect().stats().keys())
+    return list(app.control.inspect().ping().keys())
 #print(get_online_worker(app))
-app.control.inspect().active_queues()
-app.control.inspect().stats()
-app.control.inspect().clock()
-app.control.inspect().ping()
+# app.control.inspect().active_queues()
+# app.control.inspect().stats()
+# app.control.inspect().clock()
+# app.control.inspect().ping()
 
 
-def heart_beat():
+def heart_beat_start():
     global heart_beat_timer
     print("heart_beat",app.control.inspect().ping())
     heart_beat_timer = threading.Timer(__heart_beat_interval__,heart_beat)
